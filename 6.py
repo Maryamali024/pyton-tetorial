057f66f3c4e2aa073865d284a7a366a069abe158
Maryam <maryamalimaan024@gmail.com>
2026-04-11 22:13:05 +0500

    learning python

---
+++ 6.py
@@ -0,0 +1,19 @@
+a = 1
+print(a)
+c= 4
+b="Maryam"
+print (a+c)
+print (b)
+
+print("The type of a is",type(a))
+print("The type of c is",type(c))
+
+print("The type of b is",type(b))
+
+list1 = [8,5,9,[-4,5],["apple","banana"]]
+print(list1)
+
+
+
+
+
---
+++ 8.py
@@ -0,0 +1,3 @@
+a=76
+b=98
+print("The value of",a,"+",98,"is:",a+b)
