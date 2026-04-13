3fedd27635df9b49dff9fc7d9bc9dda32073eaf4
Maryam <maryamalimaan024@gmail.com>
2026-04-13 22:54:32 +0500

    python

---
+++ 9.py
@@ -0,0 +1,24 @@
+# a = "1"
+# b = "2"
+# print (int(a)+int(b))
+
+a = "1"
+b = "2"
+print (a+b)
+
+a = "1 abc"
+b = "2"
+print (int(a)+int(b))
+
+a = "1.1"
+b = "2"
+print (float(a)+int(b))
+
+a = 1.1
+b = 2
+print (a+b)
+
+c = 1.9
+d = 8
+
+print(c+d)
