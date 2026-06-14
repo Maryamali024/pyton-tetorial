1ba2b89da947522c020945981a3ee89ea38ccf28
Maryam <maryamalimaan024@gmail.com>
2026-06-14 08:19:34 +0500

    function

---
+++ function.py
@@ -0,0 +1,20 @@
+# def Greet():
+#     print("Hello Everyone")
+# Greet()
+
+# def greet():
+#     print("Hello Customer")
+# greet()
+
+# def sum():
+#     print("invalid")
+# sum()
+
+# def sum(x):
+#     return x
+# print (sum(3+2))
+
+# name = "maryam"
+# print(f"my name is {name}")
+
+
